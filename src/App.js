bda5807247d87b53863bfa20c29e09f36ada7a94
mu-kumaran <mu.kumaran1991@gmail.com>
2025-05-22 18:44:46 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function CurrentWeatherApp()
     var apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
     axios.get(apiurl).then(
       (res) => {
-        // console.log(res.data)
+        console.log(res.data)
         setOutput(res.data)
       }
     ).catch((err)=>{
@@ -31,12 +31,13 @@ function CurrentWeatherApp()
     <title>Current Weather App</title>
     <h1><span className="text-primary">Welcome to Current Weather App</span></h1>
     <p><b>This app gives the current weather data of your cities</b></p>
-    <label><b>Enter your city:</b> </label>
+    <label><b>Enter your city :</b></label>&nbsp;
     <input type="text" onChange={(e)=>cityfun(e)} placeholder="Enter city name" value={city}></input>
     <br></br><br></br>
     <button onClick={showreport} className="btn btn-success">Get weather report</button>
     <br></br><br></br>
-    {output !== null && output.cod === 200 &&
+    
+    {output !== null && output.cod === 200 && output.wind.gust !==undefined &&
     <div >
       <h3><u>Weather Report:</u></h3>
       <h4>{"Status: "+output.weather[0].main}</h4>
@@ -44,6 +45,19 @@ function CurrentWeatherApp()
       <h4>{"Wind speed: "+output.wind.speed+" m/sec"}</h4>
       <h4>{"Wind direction: "+output.wind.deg+" degrees"}</h4>
       <h4>{"Wind gust: "+output.wind.gust+" m/sec"}</h4>
+      <h4>{"Temperature: "+`${(output.main.temp - 273.15).toFixed(1)}`} &deg;C</h4>
+      <h4>{"Pressure: "+output.main.pressure+" hPa"}</h4>
+      <h4>{"Humidity: "+output.main.humidity+" %"}</h4>
+    </div> 
+    }
+
+    {output !== null && output.cod === 200 && output.wind.gust === undefined &&
+    <div >
+      <h3><u>Weather Report:</u></h3>
+      <h4>{"Status: "+output.weather[0].main}</h4>
+      <h4>{"Description: "+output.weather[0].description}</h4>
+      <h4>{"Wind speed: "+output.wind.speed+" m/sec"}</h4>
+      <h4>{"Wind direction: "+output.wind.deg+" degrees"}</h4>
       <h4>{"Temperature: "+`${(output.main.temp - 273.15).toFixed(1)}`} &deg;C</h4>
       <h4>{"Pressure: "+output.main.pressure+" hPa"}</h4>
       <h4>{"Humidity: "+output.main.humidity+" %"}</h4>
